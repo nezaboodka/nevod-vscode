50d6d18b7675c0e32793a7b345d01b9d6870cf3a
Ilya Trapashko <miningilya2@gmail.com>
2024-05-10 10:50:15 +0300

    Skip symbolic links when enumerating files in workspace

--- source/server/Services/WorkspaceServices.cs
+++ source/server/Services/WorkspaceServices.cs
@@ -1,19 +1,20 @@
 using System;
 using System.IO;
+using System.IO.Enumeration;
 
 namespace Nezaboodka.Nevod.Services
 {
     public class WorkspaceServices : AbstractServices
     {
         private readonly string _baseDirectoryPath;
-        
+
         public WorkspaceServices(string baseDirectoryPath)
         {
             _baseDirectoryPath = baseDirectoryPath;
             OpenDocumentsInDirectoryRecursively(baseDirectoryPath);
             RebuildSyntaxInfo();
         }
-        
+
         public override void OpenDocument(Uri uri, string text)
         {
             base.OpenDocument(uri, text);
@@ -31,14 +32,17 @@ namespace Nezaboodka.Nevod.Services
 
         private void OpenDocumentsInDirectoryRecursively(string directoryPath)
         {
-            foreach (string filePath in Directory.EnumerateFiles(directoryPath, "*.np"))
+            var options = new EnumerationOptions
+            {
+                RecurseSubdirectories = true,
+                AttributesToSkip = FileAttributes.Hidden | FileAttributes.System | FileAttributes.ReparsePoint,
+            };
+            foreach (string filePath in Directory.EnumerateFiles(directoryPath, "*.np", options))
             {
                 Uri uri = new(filePath);
                 string text = File.ReadAllText(filePath);
                 base.OpenDocument(uri, text);
             }
-            foreach (string childDirectoryPath in Directory.EnumerateDirectories(directoryPath))
-                OpenDocumentsInDirectoryRecursively(childDirectoryPath);
         }
     }
 }
